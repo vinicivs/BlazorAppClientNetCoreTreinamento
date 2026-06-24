77f48fc862b503d84e87d16192502e177ac8c0d1
vinicivs <vinicivs@gmail.com>
2026-06-24 17:44:20 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorAppClientNetCoreTreinamento/Models/Ceps.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorAppClientNetCoreTreinamento.Models
+{
+    public class Ceps
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; } = string.Empty;
+        public string Logradouro { get; set; } = string.Empty;
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string Bairro { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public string Uf { get; set; } = string.Empty;
+    }
+}
+
