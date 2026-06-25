82b6b05ff8c363818c349743025fd5ace7d7f652
vinicivs <vinicivs@gmail.com>
2026-06-25 08:14:35 -0300

    feat: CRUD Completo do Blazor Web Assembly

--- BlazorAppClientNetCoreTreinamento/Models/Ceps.cs
+++ BlazorAppClientNetCoreTreinamento/Models/Ceps.cs
@@ -1,16 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorAppClientNetCoreTreinamento.Models
 {
     public class Ceps
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Cep é obrigatório")]
+        [StringLength(9)]
         public string Cep { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O Logradouro é obrigatório")]
+        [StringLength(100)]
         public string Logradouro { get; set; } = string.Empty;
         public string? Numero { get; set; }
         public string? Complemento { get; set; }
+
+        [Required(ErrorMessage = "O bairro é obrigatório")]
+        [StringLength(100)]
         public string Bairro { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A cidade é obrigatória")]
+        [StringLength(100)]
         public string Cidade { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A Uf é obrigatória")]
+        [StringLength(2)]
         public string Uf { get; set; } = string.Empty;
     }
 }
--- BlazorAppClientNetCoreTreinamento/Program.cs
+++ BlazorAppClientNetCoreTreinamento/Program.cs
@@ -9,8 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7184/") });//https://localhost:7184/api/v1/Cep
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5183/") }); // 
-
 
 
 await builder.Build().RunAsync();
